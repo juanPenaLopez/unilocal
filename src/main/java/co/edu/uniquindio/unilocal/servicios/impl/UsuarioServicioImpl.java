48d08bd7b90bce2cdeb59b5bb6c5919686ae948b
Juan Peña <juanp.penal@uqvirtual.edu.co>
2024-03-20 19:02:33 -0500

    Se realizan lo mocks de las interfaces de los servicios

---
+++ src/main/java/co/edu/uniquindio/unilocal/servicios/impl/UsuarioServicioImpl.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.unilocal.servicios.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UsuarioServicioImpl {
+}
---
+++ src/main/java/co/edu/uniquindio/unilocal/servicios/interfaces/ModeradorServicio.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.unilocal.servicios.interfaces;
+
+public interface ModeradorServicio {
+}
