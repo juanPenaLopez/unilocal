36aab1c06f21b3e95609574607b0b6753b34c06d
David Martinez <davids.martinezl@uqvirtual.edu.co>
2024-04-18 20:09:47 -0500

    Se agrega la clase interface de ModeradorServicio

--- src/main/java/co/edu/uniquindio/unilocal/servicios/interfaces/ModeradorServicio.java
+++ src/main/java/co/edu/uniquindio/unilocal/servicios/interfaces/ModeradorServicio.java
@@ -6,29 +6,26 @@ import co.edu.uniquindio.unilocal.dto.CambioPasswordDTO;
 public interface ModeradorServicio {
 
     /**
-     * Elimina una cuenta de usuario basada en su ID.
-     *
-     * @param idCuenta el ID de la cuenta a eliminar
-     * @return un ResultadoDTO indicando el éxito o fracaso de la operación
-     * @throws Exception en caso de errores durante la operación
+     * Método para eliminar una cuenta.
+     * @param idCuenta El ID de la cuenta a eliminar.
+     * @return Un ResultadoDTO con los detalles del resultado de la operación.
+     * @throws Exception En caso de algún problema durante la operación.
      */
     ResultadoDTO eliminarCuenta(String idCuenta) throws Exception;
 
     /**
-     * Envía un enlace de recuperación de contraseña a un email específico.
-     *
-     * @param email el correo electrónico al que se enviará el enlace
-     * @return un ResultadoDTO indicando el éxito o fracaso de la operación
-     * @throws Exception en caso de errores durante la operación
+     * Método para enviar un enlace de recuperación de contraseña a un email.
+     * @param email El email al que enviar el enlace de recuperación.
+     * @return Un ResultadoDTO con los detalles del resultado de la operación.
+     * @throws Exception En caso de algún problema durante la operación.
      */
     ResultadoDTO enviarLinkRecuperacion(String email) throws Exception;
 
     /**
-     * Cambia la contraseña de un usuario.
-     *
-     * @param cambioPasswordDTO datos necesarios para cambiar la contraseña
-     * @return un ResultadoDTO indicando el éxito o fracaso de la operación
-     * @throws Exception en caso de errores durante la operación
+     * Método para cambiar la contraseña de una cuenta.
+     * @param cambioPasswordDTO El DTO con los datos necesarios para cambiar la contraseña.
+     * @return Un ResultadoDTO con los detalles del resultado de la operación.
+     * @throws Exception En caso de algún problema durante la operación.
      */
     ResultadoDTO cambiarPassword(CambioPasswordDTO cambioPasswordDTO) throws Exception;
 }
