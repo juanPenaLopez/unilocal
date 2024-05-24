67011fceb4e2f08dedeb43162ebd7931377999cd
Juan Peña <juanp.penal@uqvirtual.edu.co>
2024-05-24 14:13:31 -0500

    Se crea estructura para consultar las ciudades que están en la BD

---
+++ src/main/java/co/edu/uniquindio/unilocal/controladores/CiudadController.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.unilocal.controladores;
+
+import co.edu.uniquindio.unilocal.modelo.Ciudad;
+import co.edu.uniquindio.unilocal.servicios.interfaces.CiudadServicio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ciudad")
+@RequiredArgsConstructor
+public class CiudadController {
+
+    private CiudadServicio ciudadService;
+
+    @GetMapping("/consultar-ciudades")
+    public ResponseEntity<List<Ciudad>> obtenerCiudadesParaSelect() {
+        List<Ciudad> ciudades = ciudadService.obtenerTodasLasCiudades();
+        return ResponseEntity.ok(ciudades);
+    }
+}
